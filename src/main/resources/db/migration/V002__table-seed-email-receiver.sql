7cf194b476b61a74c949def8b56dad661b77dc39
Bruno Gaudio de Azevedo Mattos <brunogmattos22@gmail.com>
2024-02-26 20:57:30 -0300

    Adicionando Migrations de Testes para Envio de E-mails.

---
+++ src/main/resources/db/migration/V002__table-seed-email-receiver.sql
@@ -0,0 +1,12 @@
+INSERT INTO t_EMAIL_RECEIVER (EMAIL_RECEIVER_SUBJECT, EMAIL_RECEIVER_MAILBODY, EMAIL_RECEIVER_ORIGIN, EMAIL_RECEIVER_DESTINY, EMAIL_RECEIVER_COPY, EMAIL_RECEIVER_DATEREG) 
+VALUES 
+('Um Teste do seu Incrível Namorado!', '<html><body><h1>Olá Bambina! Aqui é o Robô do amor da Sua vida!</h1><p><h2>CUIDADO!</h2></p><h6>Posso Hackear seu E-mail e Ver sua conversinhas ein...Te Amo!</h6></body></html>', 'brunogmattos22@gmail.com', 'leticia.pinheiro@hotmail.com', "leticia.pinheiro@icloud.com", NOW()),
+('Boas-Vindas', '<html><body><h1>Bem-Vindo!</h1><p>Obrigado por se juntar a nós.</p></body></html>', 'brunogmattos22@gmail.com', 'brunogaudio.ml@gmail.com', 'brunogaudio.ml@gmail.com', NOW()),
+('Boas-Vindas', '<html><body><h1>Bem-Vindo!</h1><p>Obrigado por se juntar a nós.</p></body></html>', 'brunogmattos22@gmail.com', 'brunogaudio.ml@gmail.com', 'brunogaudio.ml@gmail.com', NOW()),
+('Boas-Vindas', '<html><body><h1>Bem-Vindo!</h1><p>Obrigado por se juntar a nós.</p></body></html>', 'brunogmattos22@gmail.com', 'brunogaudio.ml@gmail.com', '', NOW()),
+('Boas-Vindas', '<html><body><h1>Bem-Vindo!</h1><p>Obrigado por se juntar a nós.</p></body></html>', 'brunogmattos22@gmail.com', 'brunogaudio.ml@gmail.com', NULL, NOW()),
+('Boas-Vindas', '<html><body><h1>Bem-Vindo!</h1><p>Obrigado por se juntar a nós.</p></body></html>', 'brunogmattos22@gmail.com', 'brunogaudio.ml@gmail.com', NULL, NOW()),
+('Boas-Vindas', '<html><body><h1>Bem-Vindo!</h1><p>Obrigado por se juntar a nós.</p></body></html>', 'brunogmattos22@gmail.com', 'brunogaudio.ml@gmail.com', 'brunogaudio.ml@gmail.com', NOW()),
+('Boas-Vindas', '<html><body><h1>Bem-Vindo!</h1><p>Obrigado por se juntar a nós.</p></body></html>', 'brunogmattos22@gmail.com', 'brunogaudio.ml@gmail.com', 'brunogaudio.ml@gmail.com', NOW()),
+('Boas-Vindas', '<html><body><h1>Bem-Vindo!</h1><p>Obrigado por se juntar a nós.</p></body></html>', 'brunogmattos22@gmail.com', 'brunogaudio.ml@gmail.com', NULL, NOW()),
+('Boas-Vindas', '<html><body><h1>Bem-Vindo!</h1><p>Obrigado por se juntar a nós.</p></body></html>', 'brunogmattos22@gmail.com', 'brunogaudio.ml@gmail.com', NULL, NOW());
